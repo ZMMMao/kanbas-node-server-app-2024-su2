c23d1051f25ff6b366a17b091e0fb2246bd57ead
Zuowei Mao <mao.zu@northeastern.edu>
2024-08-20 11:59:56 -0400

    update a6

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from 'express';
+import session from "express-session";
 import mongoose from "mongoose";
 import UserRoutes from "./Users/routes.js";
 import Hello from "./Hello.js";
@@ -12,7 +13,25 @@ const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.
 mongoose.connect(CONNECTION_STRING);
 mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3001",
+    })
+);
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+  }
+app.use(session(sessionOptions));  
 app.use(express.json());
 UserRoutes(app);
 ModuleRoutes(app);
